7e5d5a3ba75012fe800fdde112d84b564c3f666f
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-11-08 14:16:20 -0300

    Corrige escopo do H2 e simplifica configuração do banco
    O 'mvn install' estava a falhar (Erro: Cannot load driver class) porque o <scope>runtime</scope> no pom.xml impedia o H2 de ser carregado na fase de testes.
    
    Esta correção remove o escopo da dependência do H2.
    
    Além disso, para simplificar a inicialização e resolver problemas de carregamento, o perfil 'test' foi removido. Toda a configuração do H2 foi movida do 'application-test.properties' para o 'application.properties' principal.

--- ecommerce/src/main/java/com/joaodev/ecommerce/resources/UserResource.java
+++ ecommerce/src/main/java/com/joaodev/ecommerce/resources/UserResource.java
@@ -1,23 +1,19 @@
 package com.joaodev.ecommerce.resources;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joaodev.ecommerce.entities.User;
-import org.springframework.web.bind.annotation.GetMapping;
-
-
-
-
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-    @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-        return ResponseEntity.ok().body(u);
-    }  
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
 }
